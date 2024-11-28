2ddc993f80b37a7abda746a99304ddea8046e4ac
Szymon <Szymon@DESKTOP-KD7PK0F>
2024-11-28 15:36:46 +0100

    #zej4

--- foo.hpp
+++ foo.hpp
@@ -8,5 +8,20 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    std::vector< char > potwor(people.size());
+    std::vector< char >::reverse_iterator rit = potwor.rbegin();
+
+    for (auto& osoba : people)
+    {
+        osoba.birthday();
+
+        if (osoba.isMonster())
+            *rit = 'n';
+        else
+            *rit = 'y';
+
+        rit++;
+    }
+
+    return potwor;
 }
